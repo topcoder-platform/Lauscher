33cd160f543c08a61102edecf031c1fe31406cae
veshu <tcveshu@gmail.com>
2018-06-12 00:54:09 +0545

    added tc-auth

--- ui/src/config/config.js
+++ ui/src/config/config.js
@@ -2,7 +2,10 @@
 const config = {
   API_URL: 'http://localhost:4000/api/v1',
   WS_URL: 'ws://localhost:4000',
-  DEFAULT_MESSAGE_COUNT: 20
+  DEFAULT_MESSAGE_COUNT: 20,
+  TC_AUTH_URL: 'https://accounts.topcoder-dev.com',
+  ACCOUNTS_APP_CONNECTOR: 'https://accounts.topcoder-dev.com/connector.html',
+  APP_URL: 'http://local.topcoder-dev.com:3000',
 };
 
 export default config;
