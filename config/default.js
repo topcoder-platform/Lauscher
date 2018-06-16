7e0411cf9a8c159261dbfecb8e756d720a90f9db
Mithun Kamath <callmekatootie@gmail.com>
2018-06-16 22:12:13 +0530

    Fix issue where tc-accounts module, relevant for topcoder based authentication, was throwing errors during build

--- config/default.js
+++ config/default.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
-  PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT || 3000,
   // see https://www.npmjs.com/package/no-kafka for available options
   KAFKA_OPTIONS: {
     connectionString: process.env.KAFKA_URL || 'localhost:9092',
--- set-env.js
+++ set-env.js
@@ -4,12 +4,16 @@ const targetPath = './ui/src/config/config.js';
 
 const envConfigFile = `
 const config = {
-  API_URL: '${process.env.API_URL || 'http://localhost:4000'}/api/v1',
-  WS_URL: '${process.env.WS_URL || 'ws://localhost:4000'}',
-  DEFAULT_MESSAGE_COUNT: ${process.env.DEFAULT_MESSAGE_COUNT || 20}
+  API_URL: ${process.env.API_URL} || 'http://localhost:3000/api/v1',
+  WS_URL: ${process.env.WS_URL} || 'ws://localhost:3000',
+  DEFAULT_MESSAGE_COUNT: ${process.env.DEFAULT_MESSAGE_COUNT} || 20,
+  TC_AUTH_URL: ${process.env.TC_AUTH_URL} || 'https://accounts.topcoder-dev.com',
+  ACCOUNTS_APP_CONNECTOR: ${process.env.ACCOUNTS_APP_CONNECTOR} || 'https://accounts.topcoder-dev.com/connector.html',
+  APP_URL: ${process.env.APP_URL} || 'http://localhost:3000',
 };
 
 export default config;
+
 `;
 
 writeFile(targetPath, envConfigFile, (err) => {
