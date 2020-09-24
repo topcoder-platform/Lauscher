e1013ce99975908f9d03f1ee65b242dbb2e1d74a
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2020-09-24 23:23:01 +0530

    changing new auth url

--- set-env.js
+++ set-env.js
@@ -7,8 +7,8 @@ const config = {
   API_URL: '${process.env.API_URL || 'http://localhost:3000'}/api/v1',
   WS_URL: '${process.env.WS_URL || 'ws://localhost:3000'}',
   DEFAULT_MESSAGE_COUNT: ${process.env.DEFAULT_MESSAGE_COUNT} || 20,
-  TC_AUTH_URL: '${process.env.TC_AUTH_URL || 'https://accounts.topcoder-dev.com'}',
-  ACCOUNTS_APP_CONNECTOR: '${process.env.ACCOUNTS_APP_CONNECTOR || 'https://accounts.topcoder-dev.com/connector.html'}',
+  TC_AUTH_URL: '${process.env.TC_AUTH_URL || 'https://accounts-auth0.topcoder-dev.com'}',
+  ACCOUNTS_APP_CONNECTOR: '${process.env.ACCOUNTS_APP_CONNECTOR || 'https://accounts-auth0.topcoder-dev.com'}',
   APP_URL: '${process.env.APP_URL || 'http://localhost:3000'}',
   ROLES: ['Administrator', 'Copilot']
 };
